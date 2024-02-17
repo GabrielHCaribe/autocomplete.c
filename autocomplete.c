88145e0be84b84c12980d731713b635d7cddf577
GabrielHCaribe <149834759+GabrielHCaribe@users.noreply.github.com>
2024-02-16 19:32:33 -0500

    Update autocomplete.c

--- autocomplete.c
+++ autocomplete.c
@@ -1,5 +1,13 @@
-// Test
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "autocomplete.h"
 
-// test
 
-//test
+void read_in_terms(term **terms, int *pnterms, char *filename);
+
+int lowest_match(term *terms, int nterms, char *substr);
+
+int highest_match(term *terms, int nterms, char *substr);
+
+void autocomplete(term **answer, int *n_answer, term *terms, int nterms, char *substr);
