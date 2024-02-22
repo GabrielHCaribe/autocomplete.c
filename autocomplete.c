1c578b2f77e4cac217ad8dc1789bb6d316da9116
NickWang1234 <156024397+NickWang1234@users.noreply.github.com>
2024-02-22 13:26:02 -0500

    Update autocomplete.c

--- autocomplete.c
+++ autocomplete.c
@@ -4,7 +4,39 @@
 #include "autocomplete.h"
 
 
-void read_in_terms(term **terms, int *pnterms, char *filename);
+void read_in_terms(struct term **terms, int *pnterms, char *filename)
+{
+    FILE *file = fopen(filename, "r");
+    if (file == NULL){
+        fprintf(stderr,"memory allocation error!");
+        exit(1);
+    }
+
+    *terms = (term *)malloc((*pnterms)*sizeof(term));//---> already allocated memory for all the terms
+    if (*terms == NULL) {
+        fprintf(stderr, "Memory allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < *pnterms; i++)
+    {
+     if (fscanf(file, "%lf  %199s",&(*terms)[i].weight,(*terms)[i].term) != 2)//----> gives the number of stuff read.
+     {
+        fprintf(stderr,"File could not be read %s\n",filename);//---> maybe don't have to print stuff.
+        exit(1);
+     }
+    }
+
+    fclose(file);
+
+
+}
+
+
+
+
+
+
 
 int lowest_match(term *terms, int nterms, char *substr);
 
