2d24e6b519ac93130038a595c656ea0efbb6fb41
NickWang1234 <156024397+NickWang1234@users.noreply.github.com>
2024-02-22 17:36:51 -0500

    Update autocomplete.c

--- autocomplete.c
+++ autocomplete.c
@@ -29,7 +29,7 @@ void read_in_terms(term **terms, int *pnterms, char *filename)
         }
         else
         {
-            
+             
         }
     }
 
@@ -37,6 +37,58 @@ void read_in_terms(term **terms, int *pnterms, char *filename)
 
     fclose(file);
 
+
+}
+
+int compare_terms(const void *a, const void *b) {
+    return strcmp(((term *)a)->term, ((term *)b)->term);
+}
+
+void read_in_terms(term **terms, int *pnterms, char *filename)
+{
+    //This first code opens the file
+    FILE *file = fopen(filename, "r");
+    if (file == NULL){
+        exit(1);
+    }
+
+    //Ths part of the code reads the the number saying how many terms are. Storing it in a pointer
+    char *line  = (char *)malloc(sizeof(char) * 200);
+    fgets(number, 200, file);
+    *pnterms = atoi(number);
+     //This mallocs an array of terms which we can now manipulate
+    (*terms) = (term *)malloc(sizeof(term)*(*pnterms));
+     
+    char buffer[300];
+    int i = 0;
+
+    while (i <= (*pnterms) && fgets(buffer,sizeof(buffer),file ))
+    {
+        //remove the trailing space
+        size_t newlineIndex = strcspn(buffer, "\n");//---> basically calculate how many characters before the line change 
+        if (newlineIndex < sizeof(buffer)) {
+            buffer[newlineIndex] = '\0';
+        }
+        // If the next character is a space keep skipping space.
+        char *space = strchr(buffer,' ');
+        while (*(space + 1) == ' '){
+            space++;
+        }
+
+        *space = '\0';
+        (*terms)[i] -> weight = atof(buffer);
+
+        // now we copy the rest of the characters, we define where we start copying,aka right after the space index 
+        strncpy((*terms)[i] -> term, space + 1, sizeof((*terms)[i] -> term)- 1);// the minus one is reserved for the null character
+        (*terms)[i] -> term[sizeof((*terms)[i] -> term)-1] = '\0';
+        i++;
+
+
+    }
+
+    fclose(file);
+    qsort(*terms, *pnterms,sizeof(term),compare_terms);
+    
 
 }
 
