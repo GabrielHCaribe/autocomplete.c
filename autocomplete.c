fa73ef47f3ee7a18823668c03f35377c098775b9
NickWang1234 <156024397+NickWang1234@users.noreply.github.com>
2024-02-22 23:33:08 -0500

    Update autocomplete.c

--- autocomplete.c
+++ autocomplete.c
@@ -38,102 +38,46 @@ void read_in_terms(term **terms, int *pnterms, char *filename)
 
 
 }
-//////////////////////////////
 
 
-int compare_terms(const void *a, const void *b) {
-    return strcmp(((term *)a)->term, ((term *)b)->term);
+/////////////////////////////////////////////////
+int lowest_match(struct term *terms, int nterms, char *substr)
+{
+    int sub_len = strlen(substr);
+    int low = 0;
+    int high = nterms -1;
+    while (low <= high){
+        int mid = (low + (high -low)) / 2;
+        char *snippet = (char *)malloc(200 *sizeof(char));
+        memmove(snippet, &(terms)[mid].term, sub_len); 
+        int comp = strcmp(snippet, substr);
+
+        if (comp  < 0){
+            low = mid + 1;
+            free(snippet);  
+        }
+        else if (comp > 0){
+            high = mid -1;
+            free(snippet);
+        }
+        else{
+            //char *snippet_before = (char *)malloc(200 *sizeof(char));
+           // memmove(snippet_before, &(terms)[mid -1].term, sub_len); 
+           // int comp_before = strcmp(snippet, substr);
+
+            while (mid > 0 && strcmp(snippet,substr) == 0){
+                mid--;
+            }
+            return mid;
+
+        }
+    }
 }
 
-void read_in_terms(term **terms, int *pnterms, char *filename)
-{
-    //This first code opens the file
-    FILE *file = fopen(filename, "r");
-    if (file == NULL){
-        exit(1);
-    }
-
-    //Ths part of the code reads the the number saying how many terms are. Storing it in a pointer
-    char *line  = (char *)malloc(sizeof(char) * 200);
-    fgets(number, 200, file);
-    *pnterms = atoi(number);
-     //This mallocs an array of terms which we can now manipulate
-    (*terms) = (term *)malloc(sizeof(term)*(*pnterms));
-     
-    char buffer[300];
-    int i = 0;
-
-    while (i <= (*pnterms) && fgets(buffer,sizeof(buffer),file ))
-    {
-        //remove the trailing space
-        size_t newlineIndex = strcspn(buffer, "\n");//---> basically calculate how many characters before the line change 
-        if (newlineIndex < sizeof(buffer)) {
-            buffer[newlineIndex] = '\0';
-        }
-        // If the next character is a space keep skipping space.
-        char *space = strchr(buffer,' ');
-        while (*(space + 1) == ' '){
-            space++;
-        }
-
-        *space = '\0';
-        (*terms)[i] -> weight = atof(buffer);
-
-        // now we copy the rest of the characters, we define where we start copying,aka right after the space index 
-        strncpy((*terms)[i] -> term, space + 1, sizeof((*terms)[i] -> term)- 1);// the minus one is reserved for the null character
-        (*terms)[i] -> term[sizeof((*terms)[i] -> term)-1] = '\0';
-        i++;
-
-
-    }
-
-    fclose(file);
-    qsort(*terms, *pnterms,sizeof(term),compare_terms);
-    
-
-}
-//////////////////////////////////////////////////
-//String Sorting Function
-    char s1[] = "Zhis is the way";
-    char s2[] = "This is the way";
-    if (strcmp(s1,s2) == 0){
-        printf("string is equal. \n");
-    }
-    else if (strcmp(s1,s2) < 0){ //less than 0 means that s1 has lower ascci value
-        printf("s1 < s2\n");
-    }
-     else if (strcmp(s1,s2) > 0){
-        printf("s1 > s2\n"); // greater than 0  means that s1 has higher ascci value
-    }
-
-    //what if the string is not equal?
-    //< 0 if the 1st non matching char has a lower ascci value in s1 than s2
-    //> 0 if the first non matching char has a higher ascii value in s1 than s2
-//////////////////////////////////////////////////
-int binary_search( int a[], int elem, int left, int right )//--> e: elem to find. left,right is the end point
-{
-     int mid = left + (right - left) / 2; //This is how we define the mid point
-     if (left > right){
-        return -1;
-     }
-     
-     if (a[mid] == elem){
-        return mid;
-     }
-     else if (a[mid] > elem){
-          return binary_search(a, elem ,left, mid - 1 );
-    }
-    else{
-        return binary_search(a, elem, mid +1,right);
-    }
-    
 
 
 
 
-
-
-int lowest_match(term *terms, int nterms, char *substr);
 
 int highest_match(term *terms, int nterms, char *substr);
 
