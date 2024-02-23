0cf69ddbb9567402f4c5f300e503dd1bce820d84
NickWang1234 <156024397+NickWang1234@users.noreply.github.com>
2024-02-22 21:40:11 -0500

    Update autocomplete.c

--- autocomplete.c
+++ autocomplete.c
@@ -92,7 +92,24 @@ void read_in_terms(term **terms, int *pnterms, char *filename)
     
 
 }
+//////////////////////////////////////////////////
+//String Sorting Function
+    char s1[] = "Zhis is the way";
+    char s2[] = "This is the way";
+    if (strcmp(s1,s2) == 0){
+        printf("string is equal. \n");
+    }
+    else if (strcmp(s1,s2) < 0){ //less than 0 means that s1 has lower ascci value
+        printf("s1 < s2\n");
+    }
+     else if (strcmp(s1,s2) > 0){
+        printf("s1 > s2\n"); // greater than 0  means that s1 has higher ascci value
+    }
 
+    //what if the string is not equal?
+    //< 0 if the 1st non matching char has a lower ascci value in s1 than s2
+    //> 0 if the first non matching char has a higher ascii value in s1 than s2
+    
 
 
 
